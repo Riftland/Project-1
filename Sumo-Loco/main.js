5f87aa3ffec9bb814110c0eeb7c02be58cd3a051
Riftland <fran.mscr@gmail.com>
2017-11-02 23:25:20 +0100

    change project name

---
+++ Sumo-Loco/main.js
@@ -0,0 +1,25 @@
+var keyPressed = {};
+var c1 = "red";
+var c2 = "green";
+//var c3 = "blue";
+var game;
+window.onload = function(){
+  //Create Canvas
+  var canvas = document.getElementById('tutorial');
+  ctx = canvas.getContext('2d');
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+
+  //Start the game!
+  game  = new Game(ctx);
+  game.start();
+
+  //Capture keydown and keyup
+  document.onkeydown = function(e){
+    keyPressed[e.keyCode] = true;
+  };
+
+  document.onkeyup = function(e){
+    delete keyPressed[e.keyCode];
+  };
+};
